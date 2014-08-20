f66f9e47e0c1d45ea21d4729ccf6b0e0919d4a35
Octavio Turra <octavio.turra@gmail.com>
2014-08-20 12:02:15 -0300

    switch to HTML5 color picker, added color2color

--- demo/support.js
+++ demo/support.js
@@ -1,8 +1,8 @@
 (function (window, undefined) {
-    var Canvas = !!document.createElement("canvas").getContext;;
-
-    if (!Canvas) {
-        var supportWarning = document.getElementsByClassName('brower-support');
-        supportWarning[0].style.display = 'block';
-    }
+//    var Canvas = !!document.createElement("canvas").getContext;;
+//
+//    if (!Canvas) {
+//        var supportWarning = document.getElementsByClassName('brower-support');
+//        supportWarning[0].style.display = 'block';
+//    }
 })(window);
